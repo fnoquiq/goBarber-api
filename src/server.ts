ec548492463c9093a3286410857c34a81b0b9edb
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-06-29 19:23:52 -0300

    :rocket: Configura TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log(':rocket: Server Started on port 3333!');
+  console.log('🚀 Server Started on port 3333!');
 });
