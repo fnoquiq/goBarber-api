8480c38a0cdd853ea4aef8ea9b53627511114351
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-07-01 14:59:49 -0300

    :rocket: Configura Multer

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-
 import authConfig from '../config/auth';
 
 interface TokenPayload {
@@ -17,11 +16,10 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT token is missing');
+    throw new Error('JTW token is missing');
   }
 
-  const [, token] = authHeader.split('');
-
+  const [, token] = authHeader.split(' ');
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
@@ -33,6 +31,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch {
-    throw new Error('Invalid JWT token');
+    throw new Error('Invalid JTW token');
   }
 }
