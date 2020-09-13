1c97eaa1b46d0f332caa445275bd46a235ce60da
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-09-13 13:28:03 -0300

    :rocket: Finalizando testes

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,7 +1,9 @@
 import { injectable, inject } from 'tsyringe';
+import { isAfter, addHours } from 'date-fns';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 import AppError from '@shared/errors/AppError';
 
 interface IRequest {
@@ -15,6 +17,7 @@ class ResetPasswordService {
     @inject('UsersRepository') private usersRepository: IUsersRepository,
     @inject('UserTokensRepository')
     private userTokensRepository: IUsersTokensRepository,
+    @inject('HashProvider') private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ token, password }: IRequest): Promise<void> {
@@ -30,7 +33,14 @@ class ResetPasswordService {
       throw new AppError('User does not exists');
     }
 
-    user.password = password;
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await this.hashProvider.generateHash(password);
 
     await this.usersRepository.save(user);
   }
