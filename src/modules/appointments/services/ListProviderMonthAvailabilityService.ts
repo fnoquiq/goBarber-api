d8cf16390636646269c7a9cb5fc03fa3157e5a67
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-09-19 08:53:05 -0300

    :rocket: Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -25,13 +26,30 @@ class ListProviderMonthAvailabilityService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    await this.appointmentsRepository.findAllInMonthFromProvider({
-      provider_id,
-      year,
-      month,
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return { day, available: appointmentsInDay.length < 10 };
     });
 
-    return [{ day: 1, available: false }];
+    return availability;
   }
 }
 
