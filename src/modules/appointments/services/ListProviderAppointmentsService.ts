c27fa293b1def31d004ae2bb3c1ad3f7f1c16109
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-10-04 16:26:07 -0300

    :rocket: Endereço das imagens

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,9 +1,9 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
-import { classToClass } from 'class-transformer';
 
 interface IRequest {
   provider_id: string;
